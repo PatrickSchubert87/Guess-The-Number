cd261e2cccbc48614e3bd8f3ebc1de9641021139
admin <admin@admins-MacBook-Pro.local>
2021-08-26 02:24:41 +0200

    output div shows list of the inserted numbers with inputed number count

--- main.js
+++ main.js
@@ -13,11 +13,25 @@ console.log(startGame());
 
 // create empty array for numbers
 const numberArray = [];
+let number = 0;
+let message = '';
 
 document.querySelector('.btn-ckeck').addEventListener('click', () => {
     const numberInput = document.querySelector('#number').value;
+    number = numberInput;
     numberArray.push(numberInput);
-    document.querySelector('.numberChecked').textContent = numberArray;
+
+    console.log(numberArray.length, number);
+
+    message = 
+    `
+    <tr class="row">
+    <td class="col numberIndex">#${numberArray.length}</td>
+    <td class="col numberChecked">${number}</td>
+    </tr>`;
+
+    document.querySelector('.output').insertAdjacentHTML("beforeend", message);
+    // document.querySelector('.output').appendChild(message);
 })
 
 // for(let i = 0; i <= 10; i++){
